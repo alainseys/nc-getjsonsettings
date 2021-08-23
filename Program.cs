a44fce94c57c4c63fb7e9b273e9a4dc03d1aeac2
Alain Seys <alain.seys@outlook.com>
2021-08-23 19:59:04 +0200

    get te settings from the json file and print those back to the console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace getjsonsettings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //call the configuration builder
+            ConfigBuild cnf = new ConfigBuild();
+
+            var FTP_SETTING = cnf.GetFtpSettings();
+            var FTP_SERVER = FTP_SETTING.FTP_SERVER;
+            var FTP_USER = FTP_SETTING.FTP_USER;
+            
+            Console.Write(FTP_SERVER);
+            Console.Write(FTP_USER);
+ 
         }
     }
 }
